cd4e19ca4c9828a5c5dc5470c9b6459a8d046ef1
Kayiira Richard W <105358381+RichyWilz@users.noreply.github.com>
2024-11-03 03:37:47 +0300

    Create login_screen.dart

---
+++ lib/login_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to a specific page
+            Navigator.pushNamed(context, '/home');
+          },
+          child: Text('Login'),
+        ),
+      ),
+    );
+  }
+}
