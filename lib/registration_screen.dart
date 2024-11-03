597604719a5a4391e7def9fa5ad10ba20d8abcd6
Kayiira Richard W <105358381+RichyWilz@users.noreply.github.com>
2024-11-03 03:41:56 +0300

    Update registration_screen.dart

--- lib/registration_screen.dart
+++ lib/registration_screen.dart
@@ -7,13 +7,36 @@ class RegistrationScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Register'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to a specific page
-            Navigator.pushNamed(context, '/home');
-          },
-          child: Text('Register'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'First Name'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Last Name'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Confirm Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to a specific page
+                Navigator.pushNamed(context, '/home');
+              },
+              child: Text('Register'),
+            ),
+          ],
         ),
       ),
     );
