081887e1c8c43d1ec227ee994f73a5b384f1a739
Kayiira Richard W <105358381+RichyWilz@users.noreply.github.com>
2024-11-03 03:39:53 +0300

    Update registration_screen.dart

--- lib/registration_screen.dart
+++ lib/registration_screen.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class RegistrationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to a specific page
+            Navigator.pushNamed(context, '/home');
+          },
+          child: Text('Register'),
+        ),
+      ),
+    );
+  }
+}
